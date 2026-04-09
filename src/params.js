f2faf5d5c0c95bdf365c9f7da678991c2b631b1d
devchap8 <62908499+devchap8@users.noreply.github.com>
2026-04-09 11:00:13 -0400

    feat: Add placeShip fn
    fix: In the gameboard constructor, the new arrays for
    the rows of the board were being initilized as referneces
    to each other, so placing an item in one row would place
    it in every row. Fixed to initialize arrays witout reference.

--- src/params.js
+++ src/params.js
@@ -30,18 +30,34 @@ class Ship {
 class Gameboard {
     constructor() {
         this.board = new Array(10);
-        this.board.fill(new Array(10), 0, 10);
+        for(let i = 0; i < 10; i++) {
+            const arr = new Array(10);
+            this.board[i] = arr;
+        }
         this.board.forEach(row => row.fill(null, 0, 10));
     }
 
     isValidMove = (size, horiz, x, y) => {
         // x and y mark the top left corner of the ship
-        if(horiz && ((x + size - 1) > 9 || y > 9)) return false
-        if(!horiz && (x > 9 || (y + size - 1) > 9)) return false
+        if(horiz && ((y + size - 1) > 9 || x > 9)) return false
+        if(!horiz && (y > 9 || (x + size - 1) > 9)) return false
         return true
+    }
+
+    placeShip = (ship, x, y) => {
+        if(!this.isValidMove(ship.size, ship.horizontal, x, y)) return false;
+        for(let i = 0; i < ship.size; i++) {
+            this.board[x][y] = ship;
+            ship.horizontal ? y++ : x++;
+        }
+        return true;
     }
 }
 
+// const board = new Gameboard();
+// let ship5 = new Ship(5, true);
+// board.placeShip(ship5, 0, 0);
+// console.log(board.board);
 
 
 const params = {Ship, Gameboard};
--- src/params.test.js
+++ src/params.test.js
@@ -40,14 +40,36 @@ describe('Ship Functions', () => {
 describe('Gameboard Functions', () => {
     const gameboard = new params.Gameboard();
 
-    test('valid moves', () => {
-        expect(gameboard.isValidMove(1, true, 0, 0)).toBe(true);
-        expect(gameboard.isValidMove(1, true, 9, 9)).toBe(true);
-        expect(gameboard.isValidMove(2, true, 9, 9)).toBe(false);
-        expect(gameboard.isValidMove(5, true, 6, 6)).toBe(false);
-        expect(gameboard.isValidMove(1, false, 0, 0)).toBe(true);
-        expect(gameboard.isValidMove(1, false, 9, 9)).toBe(true);
-        expect(gameboard.isValidMove(2, false, 9, 9)).toBe(false);
-        expect(gameboard.isValidMove(5, false, 6, 6)).toBe(false);
+    // test('valid moves', () => {
+    //     expect(gameboard.isValidMove(1, true, 0, 0)).toBe(true);
+    //     expect(gameboard.isValidMove(1, true, 9, 9)).toBe(true);
+    //     expect(gameboard.isValidMove(2, true, 9, 9)).toBe(false);
+    //     expect(gameboard.isValidMove(5, true, 6, 6)).toBe(false);
+    //     expect(gameboard.isValidMove(1, false, 0, 0)).toBe(true);
+    //     expect(gameboard.isValidMove(1, false, 9, 9)).toBe(true);
+    //     expect(gameboard.isValidMove(2, false, 9, 9)).toBe(false);
+    //     expect(gameboard.isValidMove(5, false, 6, 6)).toBe(false);
+    // });
+
+    let ship5 = new params.Ship(5, true);
+    let ship1 = new params.Ship(1, true);
+    let ship3 = new params.Ship(3, false);
+
+    test('placeShip', () => {
+        expect(gameboard.placeShip(ship5, 0, 0)).toBe(true);
+        expect(gameboard.board[0][0]).toBe(ship5);
+        expect(gameboard.board[0][4]).toBe(ship5);
+        expect(gameboard.board[0][5]).toBe(null);
+        expect(gameboard.placeShip(ship5, 6, 6)).toBe(false);
+        expect(gameboard.board[0][6]).toBe(null);
+        expect(gameboard.board[0][9]).toBe(null);
+        expect(gameboard.placeShip(ship1, 9, 9)).toBe(true);
+        expect(gameboard.board[9][9]).toBe(ship1);
+        expect(gameboard.placeShip(ship3, 9, 0)).toBe(false);
+        expect(gameboard.board[9][0]).toBe(null);
+        expect(gameboard.placeShip(ship3, 7, 0)).toBe(true);
+        expect(gameboard.board[7][0]).toBe(ship3);
+        expect(gameboard.board[9][0]).toBe(ship3);
     })
+
 });
