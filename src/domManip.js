06ae4807d54d0bcde8525c2e0fc09de750fc5b2a
devchap8 <62908499+devchap8@users.noreply.github.com>
2026-04-11 13:31:39 -0400

    feat: add character icons to dom

--- src/domManip.js
+++ src/domManip.js
@@ -1,4 +1,5 @@
 const screenList = Array.from(document.querySelectorAll(".screen"));
+const characterSidebar = document.querySelector(".character-sidebar");
 
 const changeScreen = (screenID) => {
     screenList.forEach(screen => {
@@ -7,9 +8,14 @@ const changeScreen = (screenID) => {
     });
 }
 
+const displayCharIcon = (char) => {
+    const icon = document.createElement("img");
+    icon.setAttribute("src", char.icon);
+    icon.setAttribute("alt", char.name);
+    characterSidebar.appendChild(icon);
+}
 
 
 
-
-const domManip = {changeScreen};
+const domManip = {changeScreen, displayCharIcon};
 export {domManip};
--- src/index.js
+++ src/index.js
@@ -6,3 +6,5 @@ import { domManip } from "./domManip.js";
 const singleplayerButton = document.querySelector(".singleplayer-button");
 singleplayerButton.addEventListener("click", () => domManip.changeScreen("character-select-screen"));
 
+params.characters.forEach(char => domManip.displayCharIcon(char));
+
